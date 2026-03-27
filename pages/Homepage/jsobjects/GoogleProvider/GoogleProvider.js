ac807564a4b37240a70b2c86e2323e78ba9c98b4
funnbot <dillonbayless1@gmail.com>
2026-03-27 00:45:12 +0000

    WIP

--- pages/Homepage/jsobjects/GoogleProvider/GoogleProvider.js
+++ pages/Homepage/jsobjects/GoogleProvider/GoogleProvider.js
@@ -1,8 +1,10 @@
 export default {
-	notFun: () => {},
-	googleProvider: new google_oauth_gsi.GoogleOAuthProvider({
-		clientId: "667459375894-3nkhrjpsluhp2genqkr4kl45gdbbv8hb.apps.googleusercontent.com",
-		onScriptLoadError: () => console.log('onScriptLoadError'),
-		onScriptLoadSuccess: () => console.log('onScriptLoadSuccess'),
-	})
-}
+	googleProvider: undefined 
+};
+
+const CLIENT_ID = "667459375894-3nkhrjpsluhp2genqkr4kl45gdbbv8hb.apps.googleusercontent.com"
+GoogleProvider.googleProvider = new google_oauth_gsi.GoogleOAuthProvider({
+	clientId: "667459375894-3nkhrjpsluhp2genqkr4kl45gdbbv8hb.apps.googleusercontent.com",
+	onScriptLoadError: () => console.log('onScriptLoadError'),
+	onScriptLoadSuccess: () => console.log('onScriptLoadSuccess'),
+});
