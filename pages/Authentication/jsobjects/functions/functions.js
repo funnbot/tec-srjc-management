1d94615f96417068a7ce4b6ebfe69141578a0a1d
funnbot <dillonbayless1@gmail.com>
2026-03-28 00:06:00 +0000

    WIP

---
+++ pages/Authentication/jsobjects/functions/functions.js
@@ -0,0 +1,55 @@
+export default {
+	SUPABASE_URL: "https://umawafpwexelguurmcna.supabase.co",
+	SUPABASE_PUBLISHABLE_KEY: "sb_publishable_TyOoXxO9vjcqhVm6ZRGPvg_XRFn-DwX",
+	client: undefined,
+
+
+	defaultTab: 'Sign In',
+
+	setDefaultTab: (newTab) => {
+		this.defaultTab = newTab;
+	},
+
+	generatePasswordHash: async () => {
+		return dcodeIO.bcrypt.hashSync(inp_registerPassword.text, 10);
+	},
+
+	verifyHash: async (password, hash) => {
+		return dcodeIO.bcrypt.compareSync(password, hash)
+	},
+
+	createToken: async (user) => {
+		return jsonwebtoken.sign(user, 'secret', {expiresIn: 60*60});
+	},
+
+	createClient: () => {
+		functions.client = supabase.createClient(functions.SUPABASE_URL, functions.SUPABASE_PUBLISHABLE_KEY);
+	},
+	
+	signIn: async () => {
+		// const password = inp_password.text;
+// 
+		// const [user] = await findUserByEmail.run();
+// 
+		// if (user && this.verifyHash(password, user?.password_hash)) {
+			// storeValue('token', await this.createToken(user))
+				// .then(() => updateLogin.run({
+				// id: user.id
+			// }))
+				// .then(() => showAlert('Register Success', 'success'))
+		// } else {
+			// return showAlert('Invalid emaill/password combination', 'error');
+		// }
+	},
+
+	register: async () => {
+		// const passwordHash = await this.generatePasswordHash();
+		// const [user] = await createUser.run({passwordHash});
+		// if (user) {
+			// storeValue('token', await this.createToken(user))
+			// showAlert('Register Success', 'success');
+		// } else {
+			// return showAlert('Error creating new user', 'error');
+		// }
+	},
+}
