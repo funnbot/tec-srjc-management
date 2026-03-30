9d60c05f0d4663e6386dd6e8a81165f335add7d7
funnbot <dillonbayless1@gmail.com>
2026-03-30 23:34:48 +0000

    WIP

--- pages/AuthenticateExchange/jsobjects/Auth/Auth.js
+++ pages/AuthenticateExchange/jsobjects/Auth/Auth.js
@@ -13,17 +13,19 @@ export default {
 
 	async onLoad () {
 		const code = appsmith.URL.queryParams?.code;
-		if (!Auth.isNonEmptyString(code)) {
-			await Auth.fail("No code provided in query params");
-			return;
-		}
+		// if (!Auth.isNonEmptyString(code)) {
+		// await Auth.fail("No code provided in query params");
+		// return;
+		// }
 
 		const verifier = appsmith.store['sb-umawafpwexelguurmcna-auth-token-code-verifier']
+		console.log(verifier)
 		if (!Auth.isNonEmptyString(verifier)) {
 			await Auth.fail("No code verifier found in local storage.")
 			return;
 		}
 
-		CodeExchange.run({ code, verifier });
+		const response = await CodeExchange.run({ code, verifier });
+		return response;
 	}
 }
